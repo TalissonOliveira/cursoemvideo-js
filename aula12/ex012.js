dd68765f430334b9099063f98ffdd1e9379527b0
Talisson <talissonlux@outlook.com>
2020-08-10 23:13:41 -0300

    commit aula12 Condições Aninhadas e  Múltipla (switch case)

---
+++ aula12/ex012.js
@@ -0,0 +1,10 @@
+var agora = new Date() // Pegar hora atual
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora >= 5 && hora < 12) {
+    console.log('Bom dia!')
+} else if (hora >= 12 && hora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
