38d3b95d21ff1d4d27e92b77733dde98cc1b7d86
Talisson <talissonlux@outlook.com>
2020-08-12 20:54:10 -0300

    commit aula13 Estruturas de repetições (While e Do while)

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+/* var contador = 0
+while (contador < 500) {
+    console.log(`Passo ${contador}`)
+    contador++
+} */
+
+var contador = 0
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador < 500)
