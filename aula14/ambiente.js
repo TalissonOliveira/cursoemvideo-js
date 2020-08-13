399c2273791c5407ce99e508aea8bab6586fb40d
Talisson <talissonlux@outlook.com>
2020-08-12 22:03:19 -0300

    commit aula14 Estruturas de repetições (For)

---
+++ aula14/ambiente.js
@@ -0,0 +1,17 @@
+for (var c=1; c<= 5; c++) {
+    console.log(c)
+}
+
+ /*
+ var c = 1
+ while (c<=5) {
+     console.log(c)
+     c++
+ }
+
+ var c = 1 
+ do {
+     console.log(c)
+     c++
+ } while (c<=5)
+ */
